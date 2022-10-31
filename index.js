757aebff09c9d106ada040879b1279226d5a876b
Allie Soldau <alliesoldau@gmail.com>
2022-10-31 17:45:58 -0400

    commit tree 4558207a908cd8c1a6d6d84d85a99341267d3c4e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(input = "roller-skate") {
+    return `This Saturday, I want to ${input}!`;
+}
+
+function mondayWork(input = "go to the office") {
+    return `This Monday, I will ${input}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    const innerFunction = function(parameter = "special") {
+        return `You are ${flair}${parameter}${flair}!`
+    }
+    return innerFunction;
+}
